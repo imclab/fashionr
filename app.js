8cc4dc529541dac8e595674af53ade47d0909e1c
Alastair Coote <alastair.coote@gmail.com>
2013-02-02 15:02:10 -0500

    Mobile view

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ var express = require('express'),
     server = require('http').createServer(app)
     io = require('socket.io').listen(server)
 
-server.listen(22727);
+server.listen(5000);
 
 app.use(express.static(__dirname + "/public"));
+
+app.use("/phone",express.static(__dirname + "/phone"));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
-
+/*
 app.get('/phone', function (req, res) {
   res.sendfile(__dirname + '/public/phone.html');
 });
-
+*/
 io.sockets.on('connection', function (socket) {  
   socket.on('click', function(data){
 
