0d6608a3d2ba042a5cbcbd8898ae98dfcdf6bb8c
Abe Stanway <abestanway@gmail.com>
2013-02-02 16:07:57 -0500

    Meh

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express'),
     app = express(),
     st = require('node-static'),
     server = require('http').createServer(app)
-    io = require('socket.io').listen(server)
+    io = require('socket.io').listen(server),
+    request = require('request');
 
 server.listen(5000);
 
@@ -12,12 +13,23 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
 });
 
+app.get('/dashboard', function (req, res) {
+  res.sendfile(__dirname + '/public/dashboard.html');
+});
+
 app.get('/phone', function (req, res) {
   res.sendfile(__dirname + '/public/phone.html');
 });
 
 io.sockets.on('connection', function (socket) {  
   socket.on('click', function(data){
+      request('https://push.geckoboard.com/v1/send/26572-70a73279121e58830beb6889bc397b63', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body) // Print the google web page.
+        }
+      })
+
+      console.log(data)
       socket.broadcast.emit('triggerPinterest', data);
   })
 });
