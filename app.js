2eb4883eac007c6a6714444dd4ca5a85ae31d6c0
Abe Stanway <abestanway@gmail.com>
2013-02-03 12:09:58 -0500

    ping fix

--- app.js
+++ app.js
@@ -21,14 +21,6 @@ app.get('/dashboard', function (req, res) {
 
 io.sockets.on('connection', function (socket) {  
   socket.on('click', function(data){
-      pingGraph(data)
-      request('https://push.geckoboard.com/v1/send/26572-70a73279121e58830beb6889bc397b63', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log(body) // Print the google web page.
-        }
-      })
-
-      console.log(data)
       socket.broadcast.emit('triggerPinterest', data);
   })
 });
